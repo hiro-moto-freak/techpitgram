1b05acddf5f0bddb81f26c08c90da51060db766d
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-03-08 23:44:18 +0900

    プロフィールページの作成

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2022_03_08_033437) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
+    t.string "profile_photo"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
