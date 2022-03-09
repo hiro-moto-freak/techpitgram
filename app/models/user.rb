2bedd483ac3d3ab993b7a54d510ef0caee48c745
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-03-09 22:41:11 +0900

    アソシエーションとバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
 
   has_many :posts, dependent: :destroy
+  has_many :likes,
 
   def update_without_current_password(params, *options)
 
